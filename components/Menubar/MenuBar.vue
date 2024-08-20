2c730eb339f98589dd1abec731348216082c3bce
aaroncdx <aaroncdx@gmail.com>
2024-08-20 23:41:32 +0800

    [BOA-2] - new pages added

---
+++ components/Menubar/MenuBar.vue
@@ -0,0 +1,8 @@
+<template>
+
+</template>
+
+<script lang="ts" setup>
+
+</script>
+<script src="./MenuBar.scss" lang="scss" scoped></script>
---
+++ pages/dashboard/index.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+
+<script src="./dashboard.scss" lang="scss" scoped></script>
